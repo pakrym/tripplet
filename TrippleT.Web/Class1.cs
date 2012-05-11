2b4c66b4ee599bc4f3fed786f6990f0730d37205
pavel <pavel@krymets.com>
2012-05-11 10:15:34 +0300

    initial commit

---
+++ TrippleT.Web/Class1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SignalR;
+using SignalR.Hubs;
+
+namespace TrippleT.Web
+{
+
+
+    public class Game
+    {
+        private readonly int _fieldSize;
+
+        public Game(string playera, string playerb, int fieldSize)
+        {
+            _fieldSize = fieldSize;
+            Players = new[] { playera, playerb };
+            Field = new byte[fieldSize, fieldSize, fieldSize];
+        }
+
+        public string[] Players { get; set; }
+        public byte[, ,] Field { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="playerId"></param>
+        /// <param name="x">tile x coordinate</param>
+        /// <param name="y">tile y coordinate</param>
+        /// <param name="z">tile z coordinate</param>
+        /// <returns></returns>
+        public bool Put(string playerId, int x, int y, int z)
+        {
+            if ((x < 0) || (x >= _fieldSize) ||
+                (y < 0) || (y >= _fieldSize) ||
+                (z < 0) || (z >= _fieldSize))
+            {
+                return false;
+            }
+            if (Field[x, y, z] != 0) return false; // tile already taken
+
+            var pi = Array.IndexOf(Players, playerId);
+            if (pi == -1) // player is not from this game, hm...
+            {
+                return false;
+            }
+
+            Field[x, y, z] = (byte)(pi + 1); // zero is nothing
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if one of the players won
+        /// </summary>
+        /// <returns>playerId if someone won, -1 if no one did</returns>
+        public int CheckVictory()
+        {
+            return -1;
+        }
+
+    }
+
+    public class GameProvider
+    {
+        static GameProvider()
+        {
+            Current = new GameProvider();
+        }
+
+        private Dictionary<string, Game> _games;
+
+        public GameProvider()
+        {
+            _games = new Dictionary<string, Game>();
+        }
+
+        public static GameProvider Current { get; private set; }
+
+        public void NewGame(string pa, string pb, int size)
+        {
+            var game = new Game(pa, pb, size);
+            if (_games.ContainsKey(pa)) _games.Remove(pa);
+            if (_games.ContainsKey(pb)) _games.Remove(pb);
+
+            _games.Add(pa, game);
+            _games.Add(pb, game);
+        }
+
+        public Game Get(string player)
+        {
+            Game g;
+            if (_games.TryGetValue(player, out g))
+            {
+                return g;
+            }
+            return null;
+        }
+
+        public void Destroy(string player)
+        {
+            var game = Get(player);
+            if (game != null)
+            {
+                foreach (var p in game.Players)
+                {
+                    _games.Remove(p);
+                }
+            }
+        }
+
+    }
+
+
+    public class Player
+    {
+        public String Name { get; set; }
+        public String PlayerId { get; set; }
+    }
+
+    public class PlayerRepository
+    {
+        static PlayerRepository()
+        {
+            Current = new PlayerRepository();
+
+        }
+
+        public static PlayerRepository Current { get; private set; }
+        private Dictionary<string, string> _names;
+        private Dictionary<string, string> _offers;
+
+        public PlayerRepository()
+        {
+            _names = new Dictionary<string, string>();
+            _offers = new Dictionary<string, string>();
+        }
+
+        public void Enter(string playerId, string name)
+        {
+            if (_names.ContainsKey(playerId)) _names.Remove(playerId);
+            _names.Add(playerId, name);
+        }
+
+        public void Exit(string playerId)
+        {
+            _names.Remove(playerId);
+        }
+
+        public void AddOffer(string playerA, string playerB)
+        {
+            if (!_offers.ContainsKey(playerA))// dont ovewrrite current offer
+                _offers.Add(playerA, playerB);
+        }
+
+        public void RemoveOffer(string playerId)
+        {
+            _offers.Remove(playerId);
+        }
+
+        public string GetOffer(string playerId)
+        {
+            string playerB;
+            if (_offers.TryGetValue(playerId, out playerB))
+                return playerB;
+            return null;
+
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> AllNames { get { return _names; } }
+    }
+
+    public class TickTacToe : Hub, IDisconnect
+    {
+        private Random _random = new Random();
+        public void Move(int x, int y, int z)
+        {
+            var playerId = this.Context.ConnectionId;
+
+            var game = GameProvider.Current.Get(playerId);
+            //don't update is move  was not valid
+            if (game.Put(playerId, x, y, z))
+            {
+                foreach (var player in game.Players)
+                {
+                    Clients[player].UpdateMove(playerId, x, y, z);
+                }
+            }
+        }
+
+        public void MakeOffer(string playerId)
+        {
+
+            var player2Id = this.Context.ConnectionId;
+            Clients[playerId].Offer(player2Id);
+            PlayerRepository.Current.AddOffer(playerId, player2Id);
+        }
+
+        public void Accept()
+        {
+
+            var playerId = this.Context.ConnectionId;
+            var offer = PlayerRepository.Current.GetOffer(playerId);
+            if (offer == null) return; // offer was canceled
+            PlayerRepository.Current.RemoveOffer(playerId);
+            GameProvider.Current.NewGame(playerId, offer, 4);
+            if (_random.NextDouble() > 0.5)
+            {
+                Clients.Game(offer, playerId);
+            }
+            else
+            {
+                Clients.Game(playerId, offer);
+            }
+
+        }
+
+        public void Decline()
+        {
+            PlayerRepository.Current.RemoveOffer(this.Context.ConnectionId);
+        }
+
+        public void Register(string name)
+        {
+            var playerId = this.Context.ConnectionId;
+
+            PlayerRepository.Current.Enter(playerId, name);
+            Clients.Enter(playerId, name);
+
+            // DIRTY HACK BELOW
+            // send all online player names using Enter method
+            // skip just loged in player name, we've already sent it
+            var c = Clients[playerId];
+            foreach (var n in PlayerRepository.Current.AllNames)
+            {
+                if (n.Key == playerId) continue;
+                c.Enter(n.Key, n.Value);
+            }
+
+
+        }
+
+
+        public Task Disconnect()
+        {
+            var playerId = Context.ConnectionId;
+
+            var g = GameProvider.Current.Get(playerId);
+            Clients.Left(playerId);
+            GameProvider.Current.Destroy(playerId);
+            PlayerRepository.Current.Exit(playerId);
+
+            return null;
+        }
+    }
+}
---
+++ TrippleT.Web/Scripts/TrippleT.js
@@ -0,0 +1,233 @@
+﻿TTT = {
+    // current player id
+    id: "",
+    hub: null,
+    fieldSize: 4,
+
+    // is in 3d mode
+    d3: true,
+    //game state: 
+    // 0 - no game
+    // 1 - my move
+    // 2 - opponent move
+    // 3 - end game???
+    state: 0,
+    clearField: function () {
+        var self = this;
+        self.field = new Array(self.fieldSize);
+        for (var i = 0; i < self.fieldSize; i++) {
+            self.field[i] = new Array(self.fieldSize);
+            for (var j = 0; j < self.fieldSize; j++) {
+                self.field[i][j] = new Array(self.fieldSize);
+            }
+        }
+    },
+
+    startGame: function (opponent, whoFirst) {
+        var self = this;
+        self.opponent = opponent;
+        self.clearField();
+        self.showGameScreen();
+        self.toggle3d(true); // will generate field
+        self.marks = {};
+        self.marks[whoFirst] = 1; // first move has X
+
+        if (whoFirst == self.id) { // other player O
+            self.marks[opponent] = 2;
+            self.myMove();
+        }
+        else {
+            self.marks[self.id] = 2;
+            self.opponentMove();
+        }
+
+    },
+
+    myMove: function () {
+        this.state = 1;
+        $("#move").text("your");
+    },
+    opponentMove: function () {
+        this.state = 2;
+        $("#move").text("opponents");
+    },
+
+
+    tryUpdateTile: function (x, y, z, tile) {
+        var self = this;
+        if (!tile)
+            tile = $("#t_" + x + "_" + y + "_" + z);
+        if (self.field) {
+            var mark = self.field[x][y][z];
+            if (mark == 1) { tile.toggleClass("x", true); }
+            if (mark == 2) { tile.toggleClass("o", true); }
+        }
+    },
+
+    toggle3d: function (enable) {
+        var self = this;
+        if (!enable) {
+            enable = !self.d3;
+        }
+
+        self.d3 = enable;
+
+        var block = $(".block");
+        block.toggleClass("span4", self.d3);
+        block.toggleClass("offset4", self.d3);
+        block.toggleClass("block-3d", self.d3);
+
+
+        block.toggleClass("span14", !self.d3);
+
+        //HACK: remove and put back planes of Chrome will 
+        // break hover event on top part of plane 
+        // see: http://stackoverflow.com/questions/10534697/hover-works-only-on-lower-part-of-rotatex-transformed-div
+        $(".plane").remove();
+        self.generatefield();
+
+
+
+    },
+
+    generatefield: function () {
+        var self = this;
+        var field = $("#field");
+        for (var i = 0; i < self.fieldSize; i++) {
+            var p = $('<div class="plane"/>');
+            p.toggleClass("d3", self.d3);
+            p.css({ top: i * -100 });
+
+            for (var j = 0; j < self.fieldSize * self.fieldSize; j++) {
+                var y = Math.floor(j / self.fieldSize);
+                var z = j % self.fieldSize;
+
+                var t = $('<div class="tile"/>')
+                                    .attr("id", "t_" + i + "_" + y + "_" + z)
+									.data({ x: i, y: y, z: z })
+									.click(function () { self.tileClick(this); })
+									.appendTo(p);
+                self.tryUpdateTile(i, y, z, t);
+            }
+            p.appendTo(field);
+
+            $('<div style="clear:both;"/>').appendTo(p);
+        }
+    },
+
+    tileClick: function (tile) {
+        var self = this;
+        if (self.state == 1) // my move
+        {
+            var pos = $(tile).data();
+            self.hub.move(pos.x, pos.y, pos.z);
+            this.opponentMove();
+        }
+    },
+
+    updateMove: function (id, x, y, z) {
+        // do i realy need this check?
+        if (this.opponent != id && this.id != id) return;
+
+        this.field[x][y][z] = this.marks[id];
+        this.tryUpdateTile(x, y, z);
+        if (this.opponent == id)// i was oppoonent's move, no just update of yours
+            this.myMove();
+    },
+
+    showPlayers: function () {
+        var self = this;
+        $("#myName").text(self.name);
+        self.loginScreen.toggle(false);
+        self.playersScreen.toggle(true);
+    },
+
+    showGameScreen: function () {
+        var self = this;
+        self.playersScreen.toggle(false);
+        self.gameScreen.toggle(true);
+
+    },
+
+    showOffer: function (id) {
+        var self = this;
+
+        $("#offerName").text($("#pl_" + id).text());
+        self.state = 4;
+        self.offerBox.toggle(true);
+    },
+
+    playerClick: function (id) {
+        var self = this;
+        if (id != self.id)
+            self.hub.makeOffer(id);
+    },
+
+    init: function () {
+        var self = this;
+
+        self.loginScreen = $("#loginScreen");
+        self.playersScreen = $("#playersScreen");
+        self.gameScreen = $("#gameScreen");
+        self.offerBox = $("#offerBox");
+
+        var connection = $.connection;
+        var hub = connection.tickTacToe;
+
+
+        hub.Enter = function (id, name) {
+
+            if (id == self.id)// we are in!
+            {
+                self.name = name;
+                self.showPlayers();
+            } else {
+                $("#noOne").remove();
+            }
+
+            $("<li>")
+                .append($('<a href="#"> ')
+                        .click(function () { self.playerClick(id); })
+                        .attr("id", "pl_" + id)
+                        .text(name)).appendTo($("#playerList"));
+        };
+        hub.Left = function (id) {
+            $("#pl_" + id).remove();
+        };
+
+        hub.Game = function (ida, idb) {
+            if (ida == self.id) {
+                self.startGame(idb, ida);
+            }
+            if (idb == self.id) {
+                self.startGame(ida, ida);
+            }
+        };
+
+        hub.Offer = function (id) {
+            self.showOffer(id);
+        };
+
+        hub.UpdateMove = function (id, x, y, z) {
+            self.updateMove(id, x, y, z);
+        };
+
+        connection.hub.start().then(function () {
+            self.id = connection.hub.id;
+        });
+
+        $("#acceptOffer").click(function () {
+            hub.accept(); $(this).parent().toggle();
+        });
+        $("#declineOffer").click(function () {
+            hub.accept(); $(this).parent().toggle();
+        });
+
+        $("#enterButton").click(function () {
+            hub.register($("#playerName").val());
+        });
+
+        self.hub = hub;
+    }
+
+};
